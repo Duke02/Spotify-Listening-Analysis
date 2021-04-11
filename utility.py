740b6bc5015e6bf63f6592786075283ec810df4b
Trystan May <trystanmay@gmail.com>
2021-04-11 09:50:39 -0500

    Saving progress from yesterday

--- utility.py
+++ utility.py
@@ -1,8 +1,11 @@
 import os.path
 import json
 import typing as tp
+import spotipy as sp
+from spotipy.oauth2 import SpotifyClientCredentials
 
 FILE_PATH: str = os.path.join('.', 'MyData')
+SPOTIFY_CREDENTIALS_PATH: str = os.path.join('.', 'spotify_creds.json')
 
 def get_spotify_data(file_type: str) -> tp.List[tp.Dict]:
     files = [f.path for f in os.scandir(FILE_PATH) if file_type in f.name]
@@ -11,3 +14,11 @@ def get_spotify_data(file_type: str) -> tp.List[tp.Dict]:
         with open(file_name, 'r') as f:
             json_objs.extend(json.load(f))
     return json_objs
+
+
+def get_spotify_client():
+    with open(SPOTIFY_CREDENTIALS_PATH, 'r') as f:
+        creds_json = json.load(f)
+        creds = SpotifyClientCredentials(client_id=creds_json['client_id'], client_secret=creds_json['client_secret'])
+        token = creds.get_access_token()
+        return sp.Spotify(auth=token)
